3d330a3166eb4f3090287c264cfee650b4f21116
lelan-j <lelan-j@mgdis.fr>
2016-05-06 15:05:15 +0200

    feat: add nested property and multi-valued property

--- src/middlewares/save.js
+++ src/middlewares/save.js
@@ -8,43 +8,123 @@ var error = require('../utils/error');
 
 var serviceName = 'data-repository';
 var logger = winston.loggers.get(serviceName);
+var columnIdentifier = '_id';
 
-function manageTypes(table, json) {
-  Object.keys(json).forEach(function (key) {
-    var type = typeof json[key];
-    switch (type) {
-      case 'boolean':
-        table.boolean(key);
-        break;
-      case 'number':
-        table.decimal(key);
-        break;
-      case 'integer':
-        table.integer(key);
-        break;
-      case 'array':
-        //TODO JLL : need to implement this behavior
-        break;
-      default:
-        table.string(key);
-        break;
-    }
-  });
-}
+/**
+ * Save into database the json object.
+ * If the table exists then alter it with json properties
+ * Else create the table based on tableName and json properties
+ * @param db
+ * @param tableName
+ * @param json
+ * @returns {Promise|*}
+ */
+function toDatabase(db, tableName, json) {
 
-function createTableSchema(db, tableName, json) {
-  return db.schema.createTable(tableName, function (table) {
-    table.increments().primary();
-    manageTypes(table, json);
-  });
-}
-
-function alterTableSchema(db, tableName, json) {
-  return db(tableName).columnInfo().then(function (columns) {
-    var newColumns = _.omit(json, Object.keys(columns));
-    return db.schema.table(tableName, function (table) {
-      manageTypes(table, newColumns);
+  /**
+   * map every object properties to a table column.
+   * generate alter table with add column if table already exists
+   * @param table{object} : KnexJS table object
+   * @param object{object} : could be plain old JSON (on createTable) or only newly properties (on alterTable)
+   */
+  function manageTypes(table, object) {
+    Object.keys(object).forEach(function (key) {
+      var value = object[key];
+      var type = typeof value;
+      switch (type) {
+        case 'boolean':
+          table.boolean(key);
+          break;
+        case 'number':
+          table.decimal(key);
+          break;
+        case 'integer':
+          table.integer(key);
+          break;
+        case 'object':
+          var isArray = Array.isArray(value);
+          var values = value;
+          // create or alter the table named table+"_"+key
+          var objectNameTable = tableName + '_' + key;
+          if(isArray) {
+            // generate current row identifier
+            json[columnIdentifier] = json[columnIdentifier] || uuid.v4();
+            // remove this array from json
+            delete json[key];
+            if (values.length > 0) {
+              when.map(values, function(val) {
+                // modify each array values with current row identifier
+                val._fid = json[columnIdentifier];
+                return toDatabase(db, objectNameTable, val);
+              });
+            }
+          } else {
+            // generate a foreign key
+            var id = uuid.v4();
+            // prepare current table with a string key (should contain the new foreign key)
+            table.string(key);
+            // set the foreign key to current json
+            json[key] = id;
+            // need to force the nested id object
+            value.id = id;
+            // create the nested table and insert the modified nested object
+            toDatabase(db, objectNameTable, value);
+          }
+          break;
+        default:
+          table.string(key);
+          break;
+      }
     });
+  }
+
+  /**
+   * Create a new table named
+   * Set a technical identifier column
+   * @returns {Promise}
+   */
+  function createTableSchema() {
+    return db.schema.createTable(tableName, function (table) {
+      // generate technical identifier
+      json[columnIdentifier] = json[columnIdentifier] || uuid.v4();
+      manageTypes(table, json);
+    }).catch(function(e) {
+      // TODO JLL : could be better to synchronize the createTable to avoid the catch exception
+      logger.warn('When creating table', e);
+      alterTableSchema();
+    });
+  }
+
+  /**
+   * Alter the table if needed
+   * Compute the new properties and add them
+   * @returns {Promise|*}
+   */
+  function alterTableSchema() {
+    return db(tableName).columnInfo().then(function (columns) {
+      var newColumns = _.omit(json, Object.keys(columns));
+      return db.schema.table(tableName, function (table) {
+        manageTypes(table, newColumns);
+      });
+    });
+  }
+
+  /**
+   * Insert into the table
+   * The row identifier is given or generated
+   * @returns {*}
+   */
+  function saveIntoDatabase() {
+    // generate technical identifier
+    json[columnIdentifier] = json[columnIdentifier] || uuid.v4();
+    return db.insert([json]).into(tableName);
+  }
+
+  // check existence of TABLE for req.params.kind
+  return db.schema.hasTable(tableName).then(function(exists) {
+    // if not exist then create the table (from incoming JSON)
+    var manageTable = exists ? alterTableSchema() : createTableSchema();
+    return manageTable.then(saveIntoDatabase);
   });
 }
 
@@ -53,9 +133,10 @@ module.exports = function(db) {
     var tableName = req.params && req.params.kind;
     logger.info(path.basename(__filename) + ' - Post incoming JSON', {table: tableName});
 
-    function saveIntoDatabase() {
-      return db.insert([req.body]).into(tableName);
-    }
+    /**
+     * By default the POST response is a HTTP 201 Created
+     * @returns {*}
+     */
     function sendResponse() {
       // insert incoming JSON into TABLE(s)
       res.status(201);
@@ -63,25 +144,25 @@ module.exports = function(db) {
         saved: true
       });
     }
+
+    /**
+     * By default the POST error is a HTTP 500 Internal Server Error
+     * @param err{Error} : the response Error
+     */
     function sendError(err) {
       // insert incoming JSON into TABLE(s)
       res.status(500);
       return res.send(err);
     }
 
-    // check existence of TABLE for req.params.kind
-    db.schema.hasTable(tableName).then(function(exists) {
-      // if not exist then create the table (from incoming JSON)
-      var manageTable = exists ? alterTableSchema(db, tableName, req.body) : createTableSchema(db, tableName, req.body);
-      manageTable.then(saveIntoDatabase).then(sendResponse).catch(function(e) {
-        var msg = 'When saving into database';
-        var logRef = uuid.v4();
-        logger.error(msg, e);
-        e.message = e.message.split('\"').join('\'');
-        sendError(error(500.1, msg, {
-          internal: e
-        }, logRef));
-      });
+    toDatabase(db, tableName, req.body).then(sendResponse).catch(function(e) {
+      var msg = 'When saving into database';
+      var logRef = uuid.v4();
+      logger.error(msg, e);
+      e.message = e.message.split('\"').join('\'');
+      sendError(error(500.1, msg, {
+        internal: e
+      }, logRef));
     });
   };
 };
--- test/start.js
+++ test/start.js
@@ -70,7 +70,7 @@ it('Should insert incoming JSON object', function(done) {
   });
 });
 
-it('Should alter the table with a new string column', function(done) {
+it('Should alter the table with a new string property', function(done) {
   var table = 'test';
   var property = 'string';
   var another = 'another';
@@ -115,7 +115,7 @@ it('Should alter the table with a new string column', function(done) {
   });
 });
 
-it('Should add a new boolean column', function(done) {
+it('Should add a new boolean property', function(done) {
   var table = 'test';
   var property = 'string';
   var another = 'another';
@@ -162,7 +162,7 @@ it('Should add a new boolean column', function(done) {
   });
 });
 
-it('Should add a new iso8601 date string column', function(done) {
+it('Should add a new iso8601 date string property', function(done) {
   var table = 'test';
   var property = 'string';
   var another = 'another';
@@ -212,7 +212,7 @@ it('Should add a new iso8601 date string column', function(done) {
   });
 });
 
-it('Should add a new number column', function(done) {
+it('Should add a new number property', function(done) {
   var table = 'test';
   var property = 'string';
   var another = 'another';
@@ -265,12 +265,76 @@ it('Should add a new number column', function(done) {
   });
 });
 
-it.skip('Should manage multi-valued property', function(done) {
+it('Should manage nested property', function(done) {
   var table = 'test';
-  var multivalue = 'array';
-    var properties = [multivalue];
+  var nested = 'nested';
   var json = {};
-  json[multivalue] = [
+  var object = {
+    key: 'value',
+    key2: 'value2',
+    key3: true,
+    key4: 13
+  };
+  json[nested] = object;
+  request({
+    method: 'POST',
+    url: 'http://localhost:' + app.port + '/' + table,
+    json: json
+  }, function (err, response) {
+    logger.info('Get response %s %s', response.statusCode, response.body);
+    response.should.be.an.object;
+    response.should.have.property('statusCode', 201);
+    response.should.have.property('body');
+    response.body.should.be.an.object;
+    response.body.should.have.property('saved', true);
+    // check if table test exists
+    db.schema.hasTable(table).then(function (exists) {
+      if (!exists) {
+        done('Table ' + table + ' does not exist');
+      } else {
+        // check if string column is present
+        db.schema.hasColumn(table, nested).then(function (has) {
+          if (!has) {
+            done('Column ' + nested + ' does not exist in ' + table);
+          } else {
+            // check value
+            db.select().from(table).then(function (results) {
+              results.should.be.an.array;
+              results.length.should.be.greaterThan(0);
+              results[results.length - 1].should.have.property(nested);
+              should.exist(results[results.length - 1].nested);
+              // check if table test exists
+              var nestedTable = table + '_' + nested;
+              db.schema.hasTable(nestedTable).then(function (exists) {
+                if (!exists) {
+                  done('Table ' + nestedTable + ' does not exist');
+                } else {
+                  db.select().from(nestedTable).then(function (nestedResults) {
+                    nestedResults.should.be.an.array;
+                    nestedResults.length.should.be.greaterThan(0);
+                    nestedResults[nestedResults.length - 1].should.have.property('id', results[results.length -1][nested]);
+                    nestedResults[nestedResults.length - 1].should.have.property('key', object['key']);
+                    nestedResults[nestedResults.length - 1].should.have.property('key2', object['key2']);
+                    nestedResults[nestedResults.length - 1].should.have.property('key3', 1/*object['key3']*/);
+                    nestedResults[nestedResults.length - 1].should.have.property('key4', object['key4']);
+                    done();
+                  });
+                }
+              });
+            });
+          }
+        });
+      }
+    });
+  });
+});
+
+it('Should manage multi-valued property', function(done) {
+  var table = 'test';
+  var array = 'array';
+  var properties = [array];
+  var json = {};
+  json[array] = [
     {
       key: 'value',
       key2: 'value2',
@@ -301,20 +365,30 @@ it.skip('Should manage multi-valued property', function(done) {
         done('Table ' + table + ' does not exist');
       } else {
         // check if string column is present
-        db.schema.hasColumn(table, multivalue).then(function (has) {
-          if (!has) {
-            done('Column ' + multivalue + ' does not exist in ' + table);
+        db.schema.hasColumn(table, array).then(function (has) {
+          if (has) {
+            done('Column ' + array + ' should not exist in ' + table);
           } else {
-            // check value
-            db.select(properties).from(table).then(function (results) {
-              results.should.be.an.array;
-              results.length.should.be.greaterThan(0);
-              results[results.length-1].should.have.property(multivalue, json[multivalue]);
-              results[results.length-1].should.have.property(another, json[another]);
-              results[results.length-1].should.have.property(boolean, 0/*json[boolean]*/);
-              results[results.length-1].should.have.property(isoDate, json[isoDate]);
-              results[results.length-1].should.have.property(number, json[number]);
-              done();
+            var foreignTable = table+'_'+array;
+            db.schema.hasTable(foreignTable).then(function (exists) {
+              if (!exists) {
+                done('Table ' + foreignTable + ' does not exist');
+              } else {
+                // check value
+                db.select().from(foreignTable).then(function (results) {
+                  results.should.be.an.array;
+                  results.should.length(json[array].length);
+                  results[0].should.have.property('key', json[array][0].key);
+                  results[0].should.have.property('key2', json[array][0].key2);
+                  results[0].should.have.property('key3', 1/*json[array][0].key3*/);
+                  results[0].should.have.property('key4', json[array][0].key4);
+                  results[1].should.have.property('key', json[array][1].key);
+                  results[1].should.have.property('key2', json[array][1].key2);
+                  results[1].should.have.property('key3', 0/*json[array][1].key3*/);
+                  results[1].should.have.property('key4', json[array][1].key4);
+                  done();
+                });
+              }
             });
           }
         });
